31a62c72994bf247b9cf45d94bf35e8bb449d454
Daniel L. <daniel.llupercio@gmail.com>
2025-01-01 12:43:23 -0500

    used test case values

--- NeetCode/IntersectionOfTwoLinkedLists/getIntersectionNode.py
+++ NeetCode/IntersectionOfTwoLinkedLists/getIntersectionNode.py
@@ -20,10 +20,13 @@ headA = ListNode(4)
 headA.next = ListNode(1)
 headA.next.next = ListNode(8)
 headA.next.next.next = ListNode(4)
+headA.next.next.next.next = ListNode(5)
 
 headB = ListNode(5)
-headB.next = ListNode(0)
+headB.next = ListNode(6)
 headB.next.next = ListNode(1)
-headB.next.next.next = headA.next.next
+headB.next.next.next = ListNode(8)
+headB.next.next.next.next = ListNode(4)
+headB.next.next.next.next.next = ListNode(5)
 
-print(solution.getIntersectionNode(headA, headB).val) # 8
+print(solution.getIntersectionNode(headA, headB)) # 8
