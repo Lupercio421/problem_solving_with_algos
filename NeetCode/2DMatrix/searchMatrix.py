74cdc96cd6dbfbd41426b25944c640ef34702b1e
Daniel L. <daniel.llupercio@gmail.com>
2025-01-05 22:54:27 -0500

    updated comments on rows and columns

--- NeetCode/2DMatrix/searchMatrix.py
+++ NeetCode/2DMatrix/searchMatrix.py
@@ -2,22 +2,27 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #get the dimensions of the matrix
         ROWS, COLS = len(matrix), len(matrix[0])
-
+        
+        #binary search to find the row
         top, bot = 0, ROWS-1
         while top <= bot:
             row = (top + bot) // 2
             if target > matrix[row][-1]:
                 top = row + 1
             elif target < matrix[row][0]:
-                bot = row -1
+                bot = row - 1
             else:
                 break
         
+        #we have crossed out all the rows and the target is not found
         if not (top <= bot):
             return False
-        row = (top + bot) // 2
-        l,r = 0, COLS-1
+        
+        #move onto second binary search
+        row = (top + bot) // 2 #this is the row we found with the target value
+        l,r = 0, COLS - 1
         while l <= r:
             m = (l + r) // 2
             if target > matrix[row][m]:
