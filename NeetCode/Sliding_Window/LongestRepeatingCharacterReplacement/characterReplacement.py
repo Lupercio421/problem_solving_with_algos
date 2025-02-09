57cd9819d8531afcc0da53ff025382975e131797
Daniel L. <daniel.llupercio@gmail.com>
2025-02-09 17:31:30 -0500

    Adding a comment on why we decrement the count of the char at l index.

--- NeetCode/Sliding_Window/LongestRepeatingCharacterReplacement/characterReplacement.py
+++ NeetCode/Sliding_Window/LongestRepeatingCharacterReplacement/characterReplacement.py
@@ -7,7 +7,7 @@ class Solution:
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r],0) #return default of 0
             while (r - l + 1) - max(count.values()) > k:
-                count[s[l]] -= 1
+                count[s[l]] -= 1 #Take the count the char at the l index, and decrement by 1.
                 l += 1
             res = max(res, r - l + 1)
         return res
