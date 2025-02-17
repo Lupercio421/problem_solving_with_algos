0c901eda45519abacb252e6e0d9efa8af11fb1f2
Daniel L. <daniel.llupercio@gmail.com>
2025-02-17 15:06:35 -0500

    s1Count[index] should be decremented

--- NeetCode/Sliding_Window/PermutationInString/checkInclusion.py
+++ NeetCode/Sliding_Window/PermutationInString/checkInclusion.py
@@ -19,19 +19,22 @@ class Solution:
         for r in range(len(s1), len(s2)):
             if matches == 26:
                 return True
-            
+            print("ord(s2[r]): "+ str(ord(s2[r])))
+            print("ord('a'): " + str(ord('a')))
+            print(str(ord(s2[r]) - ord('a')))
             index = ord(s2[r]) - ord('a') #character that was just added to the window
             s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
+            if s1Count[index] == s2Count[index]:#if the char is the same in arrays, increment matches
                 matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
+            elif s1Count[index] + 1 == s2Count[index]: #by incr. this, we may have made it too large by 1, so we decrement it. That means they were equal, but with s2Count +1, matches is decrmented by 1
                 matches -= 1
             
+            # At index l, we remove the char from the window
             index = ord(s2[l]) - ord('a') #character that was just added to the window
             s2Count[index] -= 1 #character removed from the window
             if s1Count[index] == s2Count[index]:
                 matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
+            elif s1Count[index] - 1 == s2Count[index]:
                 matches -= 1
             l += 1
         return matches == 26
