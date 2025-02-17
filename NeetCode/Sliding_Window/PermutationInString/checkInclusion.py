6b833073f361cbf9cbf4e96dc5db4cc432338e49
Daniel L. <daniel.llupercio@gmail.com>
2025-02-17 14:17:41 -0500

    permutation in string O(n) solution

---
+++ NeetCode/Sliding_Window/PermutationInString/checkInclusion.py
@@ -0,0 +1,42 @@
+from typing import List
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2):
+            return False
+
+        s1Count, s2Count = [0]*26, [0]*26 #can be implemented w/ arrays, l.c a through z
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1 #ord, getting the asci values of this char. And substract from 'a'
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+        
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            
+            index = ord(s2[r]) - ord('a') #character that was just added to the window
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+            
+            index = ord(s2[l]) - ord('a') #character that was just added to the window
+            s2Count[index] -= 1 #character removed from the window
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
+
+solution = Solution()
+s1 = "ab"
+s2 = "eidbaooo"
+print(solution.checkInclusion(s1, s2)) #True
